9bab19f1ace48bf7c3f4b70c2596573c8854e9aa
Pavithra Shetty <96063249+pavithrashetty113@users.noreply.github.com>
2023-01-05 15:38:40 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,11 @@
+package technical;
+import java.util.Scanner;
+public class tech2 {
+	public static void main(String args[])
+	{
+		int x=10,y=20;
+		x=(x+y)-(y=x);
+		System.out.println(x+" "+y);
+	}
+
+}
